e20e713a55ea79f609371b6125a6956a1d44ee80
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2020-11-16 13:35:39 +0100

    Bluetooth: Audio: BASS - Replace deprecated macro
    Replace deprecated macro DEVICE_INIT with DEVICE_DEFINE.
    See https://github.com/zephyrproject-rtos/zephyr/pull/29815
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

--- subsys/bluetooth/host/audio/bass.c
+++ subsys/bluetooth/host/audio/bass.c
@@ -581,8 +581,8 @@ static int bt_bass_init(const struct device *unused)
 	return 0;
 }
 
-DEVICE_INIT(bt_tbs, "bt_bass", &bt_bass_init, NULL, NULL,
-	    APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+DEVICE_DEFINE(bt_tbs, "bt_bass", &bt_bass_init, NULL, NULL, NULL,
+	      APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEVICE, NULL);
 
 /****************************** PUBLIC API ******************************/
 void bt_bass_register_cb(struct bt_bass_cb_t *cb)
