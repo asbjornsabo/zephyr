ff1470e499125e0da3ff5a3936110686b067fa32
Emil Gydesen <emil.gydesen@nordicsemi.no>
2020-11-28 21:56:01 +0100

    Bluetooth: Audio: Add CSIS k1 key derivation function
    Adds the k1 function that derives a key used to
    encrypt the SIRK. This also implements the aes_cmac
    function used by k1.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/host/audio/csis_crypto.c
+++ subsys/bluetooth/host/audio/csis_crypto.c
@@ -4,10 +4,39 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "csis_crypto.h"
+#include <bluetooth/crypto.h>
+#include <tinycrypt/constants.h>
+#include <tinycrypt/utils.h>
+#include <tinycrypt/aes.h>
+#include <tinycrypt/cmac_mode.h>
+#include <tinycrypt/ccm_mode.h>
 
 #define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_DEBUG_CSIS_CRYPTO)
 #define LOG_MODULE_NAME bt_csis_crypto
 #include "common/log.h"
+
+static int aes_cmac(const uint8_t key[16], const uint8_t *in, size_t in_len,
+		    uint8_t *out)
+{
+	struct tc_aes_key_sched_struct sched;
+	struct tc_cmac_struct state;
+
+	/* TODO: Copy of the aes_cmac from smp.c: Can we merge them? */
+
+	if (tc_cmac_setup(&state, key, &sched) == TC_CRYPTO_FAIL) {
+		return -EIO;
+	}
+
+	if (tc_cmac_update(&state, in, in_len) == TC_CRYPTO_FAIL) {
+		return -EIO;
+	}
+
+	if (tc_cmac_final(out, &state) == TC_CRYPTO_FAIL) {
+		return -EIO;
+	}
+
+	return 0;
+}
 
 int bt_csis_sih(const uint8_t sirk[16], const uint32_t r, uint32_t *out)
 {
@@ -44,4 +73,53 @@ int bt_csis_sih(const uint8_t sirk[16], const uint32_t r, uint32_t *out)
 	memcpy(out, res, 3);
 
 	return 0;
+}
+
+/**
+ * @brief k1 deriviation function
+ *
+ * The key derivation function k1 is used to derive a key. The derived key is
+ * used to encrypt and decrypt the value of the Set Identity Resolving Key
+ * characteristic.
+ *
+ * @param n      n is 0 or more bytes.
+ * @param n_size Number of bytes in @p n.
+ * @param salt   A 16-byte salt.
+ * @param p      p is 0 or more bytes.
+ * @param p_size Number of bytes in @p p.
+ * @param out    A 16-byte output buffer.
+ * @return int 0 on success, any other value indicates a failure.
+ */
+static int k1(const uint8_t *n, size_t n_size, const uint8_t salt[16],
+	      const uint8_t *p, size_t p_size, uint8_t out[16])
+{
+	/* TODO: This is basically a duplicate of bt_mesh_k1 - Perhaps they can
+	 * be merged
+	 */
+	uint8_t t[16];
+	int err;
+
+	/*
+	 * T = AES_CMAC_SALT(N)
+	 *
+	 * k1(N, SALT, P) = AES-CMAC_T(P)
+	 */
+
+	BT_DBG("n %s", bt_hex(n, n_size));
+	BT_DBG("salt %s", bt_hex(salt, 16));
+	BT_DBG("p %s", bt_hex(p, p_size));
+
+	err = aes_cmac(salt, n, n_size, t);
+
+	BT_DBG("t %s", bt_hex(t, sizeof(t)));
+
+	if (err) {
+		return err;
+	}
+
+	err = aes_cmac(t, p, p_size, out);
+
+	BT_DBG("out %s", bt_hex(out, 16));
+
+	return err;
 }
