0c1b44983024424142116c41482615fff8774063
Emil Gydesen <emil.gydesen@nordicsemi.no>
2021-02-10 14:21:25 +0100

    Bluetooth: Audio: Update VOCS UUIDs to assigned numbers
    Updates the VOCS UUIDs to the published assigned numbers.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- include/bluetooth/uuid.h
+++ include/bluetooth/uuid.h
@@ -1370,19 +1370,10 @@ struct bt_uuid_128 {
  */
 #define BT_UUID_CSIS_RANK \
 	BT_UUID_DECLARE_16(BT_UUID_CSIS_RANK_VAL)
-/** @def BT_UUID_VOCS_VAL
- *  @brief Volume Offset Control Service value
- */
-#define BT_UUID_VOCS_VAL 0x8fd2
-/** @def BT_UUID_VOCS
- *  @brief Volume Offset Control Service
- */
-#define BT_UUID_VOCS \
-	BT_UUID_DECLARE_16(BT_UUID_VOCS_VAL)
 /** @def BT_UUID_VOCS_STATE_VAL
  *  @brief Volume Offset State value
  */
-#define BT_UUID_VOCS_STATE_VAL 0x8fbc
+#define BT_UUID_VOCS_STATE_VAL 0x2B80
 /** @def BT_UUID_VOCS_STATE
  *  @brief Volume Offset State
  */
@@ -1391,7 +1382,7 @@ struct bt_uuid_128 {
 /** @def BT_UUID_VOCS_LOCATION_VAL
  *  @brief Audio Location value
  */
-#define BT_UUID_VOCS_LOCATION_VAL 0x8fbd
+#define BT_UUID_VOCS_LOCATION_VAL 0x2B81
 /** @def BT_UUID_VOCS_LOCATION
  *  @brief Audio Location
  */
@@ -1400,7 +1391,7 @@ struct bt_uuid_128 {
 /** @def BT_UUID_VOCS_CONTROL_VAL
  *  @brief Volume Offset Control Point value
  */
-#define BT_UUID_VOCS_CONTROL_VAL 0x8fbe
+#define BT_UUID_VOCS_CONTROL_VAL 0x2B82
 /** @def BT_UUID_VOCS_CONTROL
  *  @brief Volume Offset Control Point
  */
@@ -1409,7 +1400,7 @@ struct bt_uuid_128 {
 /** @def BT_UUID_VOCS_DESCRIPTION_VAL
  *  @brief Volume Offset Audio Output Description value
  */
-#define BT_UUID_VOCS_DESCRIPTION_VAL 0x8fbf
+#define BT_UUID_VOCS_DESCRIPTION_VAL 0x2B83
 /** @def BT_UUID_VOCS_DESCRIPTION
  *  @brief Volume Offset Audio Output Description
  */
