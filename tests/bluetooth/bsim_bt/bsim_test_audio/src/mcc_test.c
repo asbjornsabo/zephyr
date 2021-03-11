b643cd198620088fdb386c13b6023932109a92a3
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2021-03-11 11:27:33 +0100

    Bluetooth: Audio: Media control - add test for parent group object.
    Add BabbleSim test for Parent Group Object ID and Parent Group Object.
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_audio/src/mcc_test.c
+++ tests/bluetooth/bsim_bt/bsim_test_audio/src/mcc_test.c
@@ -37,6 +37,7 @@ uint64_t g_current_track_object_id;
 uint64_t g_next_track_object_id;
 uint64_t g_track_segments_object_id;
 uint64_t g_current_group_object_id;
+uint64_t g_parent_group_object_id;
 
 CREATE_FLAG(ble_is_initialized);
 CREATE_FLAG(ble_link_is_ready);
@@ -47,6 +48,7 @@ CREATE_FLAG(track_segments_object_id_read);
 CREATE_FLAG(current_track_object_id_read);
 CREATE_FLAG(next_track_object_id_read);
 CREATE_FLAG(current_group_object_id_read);
+CREATE_FLAG(parent_group_object_id_read);
 CREATE_FLAG(object_selected);
 CREATE_FLAG(metadata_read);
 CREATE_FLAG(object_read);
@@ -138,6 +140,19 @@ static void mcc_current_group_obj_id_read_cb(struct bt_conn *conn, int err,
 	SET_FLAG(current_group_object_id_read);
 }
 
+static void mcc_parent_group_obj_id_read_cb(struct bt_conn *conn, int err,
+					    uint64_t id)
+{
+	if (err) {
+		FAIL("Parent Group Object ID read failed (%d)\n", err);
+		return;
+	}
+
+	printk("Parent Group Object ID read succeeded\n");
+	g_parent_group_object_id = id;
+	SET_FLAG(parent_group_object_id_read);
+}
+
 static void mcc_otc_obj_selected_cb(struct bt_conn *conn, int err)
 {
 	if (err) {
@@ -220,6 +235,18 @@ static void mcc_otc_read_current_group_object_cb(struct bt_conn *conn, int err,
 	SET_FLAG(object_read);
 }
 
+static void mcc_otc_read_parent_group_object_cb(struct bt_conn *conn, int err,
+						struct net_buf_simple *buf)
+{
+	if (err) {
+		FAIL("Parent Group Object read failed (%d)", err);
+		return;
+	}
+
+	printk("Parent Group Object read succeeded\n");
+	SET_FLAG(object_read);
+}
+
 int do_mcc_init(void)
 {
 	/* Set up the callbacks */
@@ -230,6 +257,7 @@ int do_mcc_init(void)
 	mcc_cb.next_track_obj_id_read    = &mcc_next_track_obj_id_read_cb;
 	mcc_cb.segments_obj_id_read      = &mcc_segments_obj_id_read_cb;
 	mcc_cb.current_group_obj_id_read = &mcc_current_group_obj_id_read_cb;
+	mcc_cb.parent_group_obj_id_read  = &mcc_parent_group_obj_id_read_cb;
 	mcc_cb.otc_obj_selected = &mcc_otc_obj_selected_cb;
 	mcc_cb.otc_obj_metadata = &mcc_otc_obj_metadata_cb;
 	mcc_cb.otc_icon_object  = &mcc_icon_object_read_cb;
@@ -237,6 +265,7 @@ int do_mcc_init(void)
 	mcc_cb.otc_current_track_object  = &mcc_otc_read_current_track_object_cb;
 	mcc_cb.otc_next_track_object     = &mcc_otc_read_next_track_object_cb;
 	mcc_cb.otc_current_group_object  = &mcc_otc_read_current_group_object_cb;
+	mcc_cb.otc_parent_group_object   = &mcc_otc_read_parent_group_object_cb;
 
 	/* Initialize the module */
 	return bt_mcc_init(default_conn, &mcc_cb);
@@ -520,6 +549,47 @@ void test_main(void)
 
 	if (err) {
 		FAIL("Failed to read current group object\n");
+		return;
+	}
+
+	WAIT_FOR_FLAG(object_read);
+	UNSET_FLAG(object_read);
+
+	/* Read parent group object ******************************************/
+	err = bt_mcc_read_parent_group_obj_id(default_conn);
+	if (err) {
+		FAIL("Failed to read parent group object ID: %d", err);
+		return;
+	}
+
+	WAIT_FOR_FLAG(parent_group_object_id_read);
+
+	/* TODO: Fix the instance pointer - it is neither valid nor used */
+	err = bt_otc_select_id(default_conn, bt_mcc_otc_inst(),
+			       g_parent_group_object_id);
+	if (err) {
+		FAIL("Failed to select parent group object\n");
+		return;
+	}
+
+	WAIT_FOR_FLAG(object_selected);
+	UNSET_FLAG(object_selected);
+
+	/* TODO: Fix the instance pointer - it is neither valid nor used */
+	err = bt_otc_obj_metadata_read(default_conn, bt_mcc_otc_inst(),
+				       BT_OTC_METADATA_REQ_ALL);
+	if (err) {
+		FAIL("Failed to read parent group object metadata\n");
+		return;
+	}
+
+	WAIT_FOR_FLAG(metadata_read);
+	UNSET_FLAG(metadata_read);
+
+	err = bt_mcc_otc_read_parent_group_object(default_conn);
+
+	if (err) {
+		FAIL("Failed to read parent group object\n");
 		return;
 	}
 
