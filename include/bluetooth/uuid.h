7f1f752070d0ebaa9322af27c9bca7d4b1ba914c
Emil Gydesen <emil.gydesen@nordicsemi.no>
2021-04-07 16:22:56 +0200

    Bluetooth: Audio: Remove duplicated VOCS UUIDs
    Removes duplicated VOCS UUIDs.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- include/bluetooth/uuid.h
+++ include/bluetooth/uuid.h
@@ -1545,39 +1545,6 @@ struct bt_uuid_128 {
 /** @def BT_UUID_VOCS_STATE_VAL
  *  @brief Volume Offset State value
  */
-#define BT_UUID_VOCS_STATE_VAL 0x2B80
-/** @def BT_UUID_VOCS_STATE
- *  @brief Volume Offset State
- */
-#define BT_UUID_VOCS_STATE \
-	BT_UUID_DECLARE_16(BT_UUID_VOCS_STATE_VAL)
-/** @def BT_UUID_VOCS_LOCATION_VAL
- *  @brief Audio Location value
- */
-#define BT_UUID_VOCS_LOCATION_VAL 0x2B81
-/** @def BT_UUID_VOCS_LOCATION
- *  @brief Audio Location
- */
-#define BT_UUID_VOCS_LOCATION \
-	BT_UUID_DECLARE_16(BT_UUID_VOCS_LOCATION_VAL)
-/** @def BT_UUID_VOCS_CONTROL_VAL
- *  @brief Volume Offset Control Point value
- */
-#define BT_UUID_VOCS_CONTROL_VAL 0x2B82
-/** @def BT_UUID_VOCS_CONTROL
- *  @brief Volume Offset Control Point
- */
-#define BT_UUID_VOCS_CONTROL \
-	BT_UUID_DECLARE_16(BT_UUID_VOCS_CONTROL_VAL)
-/** @def BT_UUID_VOCS_DESCRIPTION_VAL
- *  @brief Volume Offset Audio Output Description value
- */
-#define BT_UUID_VOCS_DESCRIPTION_VAL 0x2B83
-/** @def BT_UUID_VOCS_DESCRIPTION
- *  @brief Volume Offset Audio Output Description
- */
-#define BT_UUID_VOCS_DESCRIPTION \
-	BT_UUID_DECLARE_16(BT_UUID_VOCS_DESCRIPTION_VAL)
 /** @def BT_UUID_MICS_VAL
  *  @brief Microphone Control Service value
  */
